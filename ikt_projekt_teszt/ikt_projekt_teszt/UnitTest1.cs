b8ee4a3dd1406cb7fc29ab2e19b14ca24e0aedc8
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2024-05-29 18:36:10 +0200

    Oldal betöltése letesztelve, a többihez kéne a kész oldal

--- ikt_projekt_teszt/ikt_projekt_teszt/UnitTest1.cs
+++ ikt_projekt_teszt/ikt_projekt_teszt/UnitTest1.cs
@@ -1,11 +1,30 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
 namespace ikt_projekt_teszt
 {
-    public class UnitTest1
+    public class UnitTest1: IDisposable
     {
-        [Fact]
-        public void Test1()
-        {
 
+        private readonly ChromeDriver driver;
+        
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://borsodi-koppany.github.io/WEB_tesztelos_projekt/");
+        }
+
+        [Fact]
+        public void OldalBetoltott()
+        {
+            Assert.Equal("Szavak: 0", driver.FindElement(By.XPath("/html/body/div/div[1]/h1[1]")).Text);
         }
     }
 }
